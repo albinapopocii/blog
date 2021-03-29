f21f9a71799a7eb45c6e20068e0592b4e1e9b4fa
albinapopocii <77994713+albinapopocii@users.noreply.github.com>
2021-03-29 11:57:22 +0200

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(EventImage)
+admin.site.register(Event)
+admin.site.register(Calendar)
+admin.site.register(EmploymentOpportunity)
+admin.site.register(EmployeeEvidence)
+admin.site.register(Subscribe)
